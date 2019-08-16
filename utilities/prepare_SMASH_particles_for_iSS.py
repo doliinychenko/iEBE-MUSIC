70131d58b231b9bf7c536bfc1626b03a02a4435f
Dmytro Oliinychenko <doliinychenko@lbl.gov>
2019-08-16 13:07:00 -0700

    iSS sampling selector: Don't forget about anti-baryons
    Signed-off-by: Dmytro Oliinychenko <doliinychenko@lbl.gov>

--- utilities/prepare_SMASH_particles_for_iSS.py
+++ utilities/prepare_SMASH_particles_for_iSS.py
@@ -21,13 +21,17 @@ if __name__ == "__main__":
         line = infile.readline()
         if (not line or line.strip() == ""): break
         line_split = line.split()
-        pdg = line_split[0]
+        pdg = line_split[0].strip()
         ndecays = int(line_split[-1])
         mass = float(line_split[2])
         for _ in xrange(ndecays): infile.readline()
-        # Do not sample photons, too heavy particles, and nuclei
-        if (mass > 0.1 and mass < 2.3 and len(pdg) < 10):
+        # Do not sample photons, too heavy particles, and d'
+        if (mass > 0.1 and mass < 2.3 and pdg != '1000010021'):
             outfile.write(pdg + '\n')
+            # anti-baryons
+            quarks = ('000'+ pdg)[-4:-1]
+            if '0' not in quarks:
+                outfile.write('-' + pdg + '\n')
     infile.close()
     outfile.close()
     print "Created ", iss_tables_folder, " from ", iss_tables_folder
